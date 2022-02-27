09ce1673dabe9c45d48d61493b99d7a5874b54ba
Cristian Dragomir <cristiandrg96@gmail.com>
2022-02-27 19:08:07 +0200

    Implement sort and search

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
+    <input
+      placeholder="Search"
+      :value="searchTerm"
+      @input="(e) => onSearch(e.target.value)"
+    />
     <table class="ml-10">
       <tr>
         <th></th>
-        <th>#</th>
-        <th>Coin Name</th>
-        <th>Symbol</th>
-        <th>Price</th>
-        <th>Market Cap</th>
+        <th @click="() => onHeadingClick('market_cap_rank')">#</th>
+        <th @click="() => onHeadingClick('name')">Coin Name</th>
+        <th @click="() => onHeadingClick('symbol')">Symbol</th>
+        <th @click="() => onHeadingClick('current_price')">Price</th>
+        <th @click="() => onHeadingClick('market_cap')">Market Cap</th>
       </tr>
-      <tr v-for="coin in coinList" :key="coin.id">
+      <tr v-for="coin in coinsToDisplay" :key="coin.id">
         <th>
           <HeartToggle
             :favorited="GET_COIN_FAVORITE_STATUS(coin.id)"
@@ -27,6 +32,7 @@
 </template>
 
 <script>
+import { sortBy } from 'lodash'
 import { mapState, mapActions, mapGetters } from 'vuex'
 import HeartToggle from '@/components/HeartToggle'
 
@@ -35,12 +41,61 @@ export default {
   components: {
     HeartToggle,
   },
-  methods: {
-    ...mapActions(['TOGGLE_FAVORITE_COIN_ID']),
-  },
   computed: {
-    ...mapState(['coinList', 'favoriteCoinIds']),
+    ...mapState([
+      'coinList',
+      'favoriteCoinIds',
+      'searchTerm',
+      'sortColumn',
+      'sortAscending',
+      'searchResults',
+    ]),
     ...mapGetters(['GET_COIN_FAVORITE_STATUS']),
+    coinsToDisplay() {
+      let result = this.coinList
+
+      if (this.searchResults.length > 0) {
+        result = this.searchResults.map((el) =>
+          result.find((coin) => coin.id === el.ref)
+        )
+      }
+
+      if (this.sortColumn) {
+        let sortedList = sortBy(result, [this.sortColumn])
+        if (this.sortAscending) {
+          sortedList = sortedList.reverse()
+        }
+
+        return sortedList
+      }
+
+      return result
+    },
+  },
+  methods: {
+    ...mapActions([
+      'TOGGLE_FAVORITE_COIN_ID',
+      'SET_SEARCH_TERM',
+      'SET_SORT_PARAMS',
+      'SEARCH',
+    ]),
+    onSearch(query) {
+      this.SET_SEARCH_TERM(query)
+      this.SEARCH(query)
+    },
+    onHeadingClick(newSortColumn) {
+      if (this.sortColumn === newSortColumn) {
+        this.SET_SORT_PARAMS({
+          sortColumn: newSortColumn,
+          sortAscending: !this.sortAscending,
+        })
+      } else {
+        this.SET_SORT_PARAMS({
+          sortColumn: newSortColumn,
+          sortAscending: true,
+        })
+      }
+    },
   },
 }
 </script>
